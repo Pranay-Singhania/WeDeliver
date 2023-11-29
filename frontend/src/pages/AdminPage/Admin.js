8cead560c32cfc61e54e8ef5204607028cf6e03b
Pranay Singhania <pranay.s4299@gmail.com>
2023-11-30 04:51:33 +0530

    bug fixes

--- frontend/src/pages/AdminPage/Admin.js
+++ frontend/src/pages/AdminPage/Admin.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+// import
 
 const Admin = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Route path="/admin/dashboard" element=<DashBoard /> />
-        <Route path="/admin/restaurants" element=<CategoryPage /> />
-      </BrowserRouter>
-    </>
+    <>This is admin panel</>
+    // <>
+    //   <BrowserRouter>
+    //     <Route path="/admin/dashboard" element=<DashBoard /> />
+    //     <Route path="/admin/restaurants" element=<CategoryPage />
+    //   </BrowserRouter>
+    // </>
   );
 };
 
-export default adminPage;
+export default Admin;
