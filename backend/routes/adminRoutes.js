6b892dfb853926e446b8f8febfa2d69f51fbd9e1
Pranay Singhania <pranay.s4299@gmail.com>
2023-11-30 12:19:26 +0530

    bug fixes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,7 +5,10 @@ const upload = require("../middlewares/multer.middleware.js");
 const { addRestaurant } = require("../controllers/adminController/adminRestaurantController.js");
 // const { addDish, updateDish, deleteDish } = require("../controllers/adminController/adminProductController.js");
 // const { addUser, updateUser, deleteUser } = require("../controllers/adminController/adminUserController.js");
-
+router.use("/addRestaurant", (req, res, next) => {
+  console.log("POST /api/addRestaurant endpoint is hit.");
+  next(); // Move to the next middleware or route handler
+});
 router.post("/addRestaurant", upload.single("restaurantImg"), addRestaurant);
 // router.post("/updateRestaurant", updateRestaurant);
 // router.post("/deleteRestaurant", deleteRestaurant);
