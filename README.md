467a53ef122fc10fa395b54e228891d39001b94d
Pranay Singhania <pranay.s4299@gmail.com>
2023-12-01 07:43:49 +0530

    feat: updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Wedeliver
+
+<img width="948" alt="Screenshot 2023-12-01 074210" src="https://github.com/Pranay-Singhania/WeDeliver/assets/53472824/8a2e8cc3-9c5c-44dc-b9ff-dea503cb548d">
+
+A Food Delivery App with features like -
+(1) Implemented SignIn/SignUp feature.
+(2) Used Swiggy JSON format to display the content.
+(3) Implemented CRUD (Create,Read,Update, Delete) feature.
+(4) Designed an appealing UI.
+(5) Admin Panel (currently in progress - backend completed, frontend remaining).
--- frontend/src/components/SearchLocation/Map.js
+++ frontend/src/components/SearchLocation/Map.js
@@ -2,17 +2,36 @@ import { LoadScript, GoogleMap, StandaloneSearchBox } from "@react-google-maps/a
 import "./SearchLocation.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setMyAddress } from "../../store/AddressSlice";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 export default function Map(props) {
   const dispatch = useDispatch();
   const myAddress = useSelector((store) => store.myAddress.data);
+  const searchBoxRef = useRef(null); // Ref to store the StandaloneSearchBox instance
 
   const navigate = useNavigate();
-  function handleLoad() {
+  function handleLoad(searchBox) {
+    searchBoxRef.current = searchBox;
+
     console.log(process.env.REACT_APP_KEY);
     console.log("handleLoad called");
   }
+  function handlePlacesChanged() {
+    const searchBox = searchBoxRef.current;
+
+    if (searchBox) {
+      const places = searchBox.getPlaces();
+
+      if (places.length > 0) {
+        const selectedPlace = places[0];
+        const selectedAddress = selectedPlace.formatted_address;
+
+        dispatch(setMyAddress(selectedAddress));
+      }
+    }
+    console.log(myAddress);
+  }
+
   document.addEventListener("keydown", function (e) {
     // Check if the pressed key is the "Enter" key
     if (e.key === "Enter" || e.keyCode === 13) {
